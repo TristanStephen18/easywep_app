84c819fd60053d548c614ae21a39dc35643da186
JaxylViernes <144340288+JaxylViernes@users.noreply.github.com>
2025-12-02 18:35:05 +0200

    pogiko

--- src/pages/MyAccounts.tsx
+++ src/pages/MyAccounts.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 export default function TDBankAccount() {
   const [selectedAccount] = useState('TD UNLIMITED BUSINESS PLAN - 5241534 $19.67');
