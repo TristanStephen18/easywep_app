bc28deab15c7339daf5be1481ad489eb0254e95f
TristanStephen18 <trasco_21ur1406@psu.edu.ph>
2025-12-02 22:28:56 +0800

    reverted routing code

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,13 @@ import TDBankAccount from "./pages/MyAccounts";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
- 
-
   return (
-<BrowserRouter>
-<Routes>
-   <Route
-          path="/"
-          element={
-           <TDBankAccount/>
-          }
-        />
-</Routes>
-</BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TDBankAccount />} />
+      </Routes>
+    </BrowserRouter>
   );
-
 }
 
 export default App;
